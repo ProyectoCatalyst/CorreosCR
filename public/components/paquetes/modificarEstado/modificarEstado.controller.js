18219221db9e893596faa49cb5bb3fb5745bbc7e
Braulio Quirós <bquirosa@ucenfotec.ac.cr>
2018-04-18 22:49:52 -0600

    Consultar estado de paquete
    Se agrego funcionalidad al consultar estado de paquete, se elimino la directiva headline porque no se utilizaba.

--- public/components/paquetes/modificarEstado/modificarEstado.controller.js
+++ public/components/paquetes/modificarEstado/modificarEstado.controller.js
@@ -7,39 +7,66 @@
     controladorModificarEstadoPaquete.$inject = ['$stateParams', '$state', '$http', 'servicioPaquetes', 'servicioInicioSesion'];
 
     function controladorModificarEstadoPaquete($stateParams, $state, $http, servicioPaquetes, servicioInicioSesion) {
-        
+
         let vm = this;
         const userAuth = servicioInicioSesion.getAuthUser();
         if (userAuth == undefined) {
             $state.go('main.inicio');
         }
 
+        vm.rolUsuario = userAuth.getRol();
+
         const paqueteActivo = servicioPaquetes.consultarDatosSession();
         if (paqueteActivo == undefined) {
             $state.go('main.listarPaquetes');
         }
-        
+
         vm.paqueteMostrar = paqueteActivo;
 
 
-        vm.actualizarPaquete = (PaquetesPrealertadosMod) => {
+        vm.cambioEstado = (pEstado, idRepartidor) => {
+            let estadoDatos = [pEstado, idRepartidor],
+                estado = '';
 
-            let objPaqueteFormato = new Paquete(PaquetesPrealertadosMod.trackingPaquete, PaquetesPrealertadosMod.tipoPaquete,
-                PaquetesPrealertadosMod.pesoPaquete, PaquetesPrealertadosMod.precioPaquete, PaquetesPrealertadosMod.costoEnvio,
-                PaquetesPrealertadosMod.costoTotalPaquete, estadoNuevo, PaquetesPrealertadosMod.idRepartidor,
-                PaquetesPrealertadosMod.idSucursal, PaquetesPrealertadosMod.idCliente, PaquetesPrealertadosMod.idMensajero);
-
-            servicioPaquetes.modificarEstado(objPaqueteFormato); // envia todo el objeto, compara cada paquete ne el sistema con el codigo del paquete nuevo pmodificarEstado.convenio = paqueteLS[i].convenio
-
-
-            swal({
-                title: "Actualización exitosa",
-                text: "estado actualizado correctamente",
-                icon: "success",
-                button: "Aceptar"
-            });
-
-            $state.go('main.listarPaquetesPrealertados');
+            switch (estadoDatos[0]) {
+                case 0:
+                    estado = "Prealertado";
+                    paqueteActivo.setEstadoPaquete(estado);
+                    break;
+                case 1:
+                    estado = "Saliendo de aduana";
+                    paqueteActivo.setEstadoPaquete(estado);
+                    break;
+                case 2:
+                    estado = "Entregado a repartidor";
+                    paqueteActivo.setEstadoPaquete(estado);
+                    break;
+                case 3:
+                    estado = "Entregado a cliente";
+                    paqueteActivo.setEstadoPaquete(estado);
+                    paqueteActivo.setRepartidor(estadoDatos[1]); 
+                    break;
+                default:
+                    
+                    break;
+            }
+            let registro = servicioPaquetes.modificarEstado(paqueteActivo); // envia todo el objeto, compara cada paquete ne el sistema con el codigo del paquete nuevo pmodificarEstado.convenio = paqueteLS[i].convenio
+            if(registro == true){
+                swal({
+                  title: "Cambio exitoso",
+                  text: "Se actualizo el estado del paquete correctamente",
+                  icon: "success",
+                  button: "Aceptar"
+                });
+                vm.tarjetaNueva = null;
+              }else{
+                swal({
+                  title: "Ha ocurrido un Error",
+                  text: "No se ha realizado el cambio",
+                  icon: "error",
+                  button: "Aceptar"
+                });
+              }
         }
 
         vm.regresar = () => {
